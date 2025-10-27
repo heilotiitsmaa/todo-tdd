d7aecb17943a31c21707850abaf4fd9489e90687
Heilo Tiitsmaa <heilo.tiitsmaa@voco.ee>
2025-10-27 12:46:39 +0200

    Created TodoController.createTodo unit test with data and HTTP mocks

---
+++ controllers/todo.controller.js
@@ -0,0 +1,6 @@
+const TodoModel = require("../models/todo.model");
+
+exports.createTodo = async (req, res, next) => {
+  const createdModel = await TodoModel.create(req.body);
+  res.status(201).json(createdModel);
+};
